441c90980d67fa572fd7a2e96e25363483cff860
wonjw <49180503+why3E@users.noreply.github.com>
2024-06-05 14:36:04 +0900

    Initial commit

---
+++ LabProject16/Scene.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Timer.h"
+#include "stdafx.h"
+#include "Shader.h"
+#include "Camera.h"
+
+class CScene
+{
+public:
+	//씬의 모든 게임 객체들에 대한 마우스 픽킹을 수행한다.
+	CGameObject* PickObjectPointedByCursor(int xClient, int yClient, CCamera* pCamera);
+public:
+	CScene();
+	~CScene();
+	//씬에서 마우스와 키보드 메시지를 처리한다.
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
+		* pd3dCommandList);
+	void ReleaseObjects();
+	bool ProcessInput(UCHAR* pKeysBuffer);
+	void AnimateObjects(float fTimeElapsed);
+	void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
+	void ReleaseUploadBuffers();
+	//그래픽 루트 시그너쳐를 생성한다.
+	ID3D12RootSignature* CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	ID3D12RootSignature* GetGraphicsRootSignature();
+protected:
+	//씬은 게임 객체들의 집합이다. 게임 객체는 셰이더를 포함한다.
+	ID3D12RootSignature* m_pd3dGraphicsRootSignature = NULL;
+	CObjectsShader* m_pShaders = NULL;
+	int m_nShaders = 0;
+};
+
+class start_Scene :CScene
+{
+	start_Scene();
+	~start_Scene();
+
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+
+};
+
+class stage_Scene :CScene
+{
+	stage_Scene();
+	~stage_Scene();
+
+	virtual void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+};
